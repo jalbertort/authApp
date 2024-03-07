cccda5336e6bb343214f28e3a3bb7ae97063f503
jalbertort <jramireztolentino@gmail.com>
2024-03-07 12:39:22 -0400

    Add guard login and dashboard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, tap, throwError } from 'rxjs';
+
+import { environment } from '../../../environments/environments';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject( HttpClient );
+
+  private _currentUser = signal<User|null>( null );
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking );
+
+  currentUser = computed( () => this._currentUser());
+  authStatus = computed( () => this._authStatus());
+
   constructor() { }
+
+  login( email: string, password: string ): Observable<boolean> {
+
+    const url = `${ this.baseUrl }/auth/login`;
+    const body = { email, password }
+
+    return this.http.post<LoginResponse>( url, body )
+      .pipe(
+        tap( ({ user, token }) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem('token', token);
+        }),
+
+        map( () => true ),
+
+        catchError( err => throwError( () => err.error.message ) ),
+
+      )
+  }
+
 }
--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
 export class DashboardLayoutComponent {
+
+  private authService = inject( AuthService )
+  user = computed( () => this.authService.currentUser() );
 
 }
